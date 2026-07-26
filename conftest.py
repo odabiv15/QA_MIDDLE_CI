773da05536f2cbb44c8aac4ae75f7acae4211059
genrik_qa <kochkaprotbcaa@gmail.com>
2026-07-26 21:55:19 +0300

    Добавление новых файлов

--- conftest.py
+++ conftest.py
@@ -4,6 +4,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.remote.client_config import ClientConfig
 
+from utils.attache import add_logs, add_screenshot, add_video
+
 SELENOID_URL = "https://selenoid.qa.guru/wd/hub"
 SELENOID_USER = "user1"
 SELENOID_PASSWORD = "1234"
@@ -17,8 +19,13 @@ def _use_selenoid():
 def driver():
     options = Options()
     options.add_argument("--window-size=1920,1080")
+    options.set_capability("goog:loggingPrefs", {"browser": "ALL"})
 
     if _use_selenoid():
+        options.set_capability("selenoid:options", {
+            "enableVNC": True,
+            "enableVideo": True,
+        })
         client_config = ClientConfig(
             remote_server_addr=SELENOID_URL,
             username=SELENOID_USER,
@@ -33,4 +40,10 @@ def driver():
         browser = webdriver.Chrome(options=options)
 
     yield browser
+
+    session_id = browser.session_id
+    add_screenshot(browser)
+    add_logs(browser)
     browser.quit()
+    if _use_selenoid():
+        add_video(session_id)
