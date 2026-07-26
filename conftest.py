218c0e7ac8705bb72f5543250e593794ebf98995
genrik_qa <kochkaprotbcaa@gmail.com>
2026-07-26 21:35:13 +0300

    Добавление новых файлов

--- conftest.py
+++ conftest.py
@@ -2,8 +2,11 @@ import os
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.remote.client_config import ClientConfig
 
-SELENOID_URL = "https://user1:1234@selenoid.autotests.cloud/wd/hub"
+SELENOID_URL = "https://selenoid.autotests.cloud/wd/hub"
+SELENOID_USER = "user1"
+SELENOID_PASSWORD = "1234"
 
 
 def _use_selenoid():
@@ -16,9 +19,15 @@ def driver():
     options.add_argument("--window-size=1920,1080")
 
     if _use_selenoid():
+        client_config = ClientConfig(
+            remote_server_addr=SELENOID_URL,
+            username=SELENOID_USER,
+            password=SELENOID_PASSWORD,
+        )
         browser = webdriver.Remote(
             command_executor=SELENOID_URL,
             options=options,
+            client_config=client_config,
         )
     else:
         browser = webdriver.Chrome(options=options)
